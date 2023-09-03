6949bb711ac38987f0eea6e5c5c0792412c1d4f5
zhangxinshuai <xsszhang_i@didiglobal.com>
2023-09-03 16:32:43 +0800

    添加在线用户查询功能

--- user.go
+++ user.go
@@ -51,9 +51,23 @@ func (this *User) Offline() {
 	this.server.BoardCast(this, "已下线")
 }
 
+func (this *User) SendMsg(msg string) {
+	this.conn.Write([]byte(msg))
+}
+
 // 用户发消息
 func (this *User) DoMessage(msg string) {
-	this.server.BoardCast(this, msg)
+	if msg == "who" {
+		// 查询当前在线用户都有哪些
+		this.server.mapLock.Lock()
+		for _, user := range this.server.OnlineMap {
+			onlineMsg := "[" + user.Addr + "]" + user.Name + ":" + "在线...\n"
+			this.SendMsg(onlineMsg)
+		}
+		this.server.mapLock.Unlock()
+	} else {
+		this.server.BoardCast(this, msg)
+	}
 }
 
 // 监听当前user channel的方法，一单有消息，就直接发送给对端客户端
